83d8d23fcffa1967c9aff9b5e0c609502fdfac66
abcd <abcd@abcds-MacBook-Pro.local>
2024-05-06 08:58:42 -0400

    pep8 compliance

--- pylmm/misc.py
+++ pylmm/misc.py
@@ -21,42 +21,42 @@ import sys
 import numpy as np
 from pylmm.lmm import LMM
 
-def fitTwo(y,K1,K2,X0=None,wgrids=100):
-      '''
-	 Simple function to fit a model with two variance components.
-	 It works by running the standard pylmm algorithm in a loop
-	 where at each iteration of the loop a new kinship is generated
-	 as a linear combination of the original two.
-      '''
 
-      # Create a uniform grid
-      W = np.array(range(wgrids)) / float(wgrids)
-      Res = []
-      LLs = []
+def fitTwo(y, K1, K2, X0=None, wgrids=100):
+    '''
+       Simple function to fit a model with two variance components.
+       It works by running the standard pylmm algorithm in a loop
+       where at each iteration of the loop a new kinship is generated
+       as a linear combination of the original two.
+    '''
 
-      for w in W:
-	 # heritability will be estimated for linear combo of kinships
-	 K = w*K1 + (1.0 - w)*K2
-	 sys.stderr.write("Fitting weight %0.2f\n" % (w))
-	 L = LMM(y,K,X0=X0)
-	 R = L.fit()
-	 Res.append(R)
-	 LLs.append(R[-1])
-      
-	 del K
+    # Create a uniform grid
+    W = np.array(range(wgrids)) / float(wgrids)
+    Res = []
+    LLs = []
 
-      L = np.array(LLs)
-      i = np.where(L == L.max())[0]
-      if len(i) > 1:
-	 sys.stderr.write("WARNING: Found multiple maxes using first one\n")
+    for w in W:
+        # heritability will be estimated for linear combo of kinships
+        K = w * K1 + (1.0 - w) * K2
+        sys.stderr.write("Fitting weight %0.2f\n" % (w))
+        L = LMM(y, K, X0=X0)
+        R = L.fit()
+        Res.append(R)
+        LLs.append(R[-1])
 
-      i = i[0]
-      hmax,beta,sigma,LL = Res[i]
-      w = W[i]
+        del K
 
-      h1 = w * hmax 
-      h2 = (1.0 - w) * hmax 
-      e = (1.0 - hmax) 
+    L = np.array(LLs)
+    i = np.where(L == L.max())[0]
+    if len(i) > 1:
+        sys.stderr.write("WARNING: Found multiple maxes using first one\n")
 
-      return h1,h2,e,beta,sigma,LL
- 
+    i = i[0]
+    hmax, beta, sigma, LL = Res[i]
+    w = W[i]
+
+    h1 = w * hmax
+    h2 = (1.0 - w) * hmax
+    e = (1.0 - hmax)
+
+    return h1, h2, e, beta, sigma, LL
--- scripts/pylmmKinship.py
+++ scripts/pylmmKinship.py
@@ -16,10 +16,15 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from pylmm import input
+from pylmm.lmm import calculateKinship
+from scipy import linalg
+import numpy as np
+import os
 import sys
 import pdb
 
-from optparse import OptionParser,OptionGroup
+from optparse import OptionParser, OptionGroup
 usage = """usage: %prog [options] --[tfile | bfile] plinkFileBase outfile
 
 """
@@ -27,98 +32,134 @@ usage = """usage: %prog [options] --[tfile | bfile] plinkFileBase outfile
 parser = OptionParser(usage=usage)
 
 basicGroup = OptionGroup(parser, "Basic Options")
-#advancedGroup = OptionGroup(parser, "Advanced Options")
+# advancedGroup = OptionGroup(parser, "Advanced Options")
 
-#basicGroup.add_option("--pfile", dest="pfile",
+# basicGroup.add_option("--pfile", dest="pfile",
 #                  help="The base for a PLINK ped file")
 basicGroup.add_option("--tfile", dest="tfile",
-                  help="The base for a PLINK tped file")
+                      help="The base for a PLINK tped file")
 basicGroup.add_option("--bfile", dest="bfile",
-                  help="The base for a PLINK binary ped file")
-basicGroup.add_option("--emmaSNP", dest="emmaFile", default=None,
-                  help="For backwards compatibility with emma, we allow for \"EMMA\" file formats.  This is just a text file with individuals on the rows and snps on the columns.")
-basicGroup.add_option("--emmaNumSNPs", dest="numSNPs", type="int", default=0,
-		     help="When providing the emmaSNP file you need to specify how many snps are in the file")
+                      help="The base for a PLINK binary ped file")
+basicGroup.add_option(
+    "--emmaSNP",
+    dest="emmaFile",
+    default=None,
+    help="For backwards compatibility with emma, we allow for \"EMMA\" file formats.  This is just a text file with individuals on the rows and snps on the columns.")
+basicGroup.add_option(
+    "--emmaNumSNPs",
+    dest="numSNPs",
+    type="int",
+    default=0,
+    help="When providing the emmaSNP file you need to specify how many snps are in the file")
 
-basicGroup.add_option("-e", "--efile", dest="saveEig", help="Save eigendecomposition to this file.")
-basicGroup.add_option("-n", default=1000,dest="computeSize", type="int", help="The maximum number of SNPs to read into memory at once (default 1000).  This is important when there is a large number of SNPs, because memory could be an issue.")
+basicGroup.add_option(
+    "-e",
+    "--efile",
+    dest="saveEig",
+    help="Save eigendecomposition to this file.")
+basicGroup.add_option(
+    "-n",
+    default=1000,
+    dest="computeSize",
+    type="int",
+    help="The maximum number of SNPs to read into memory at once (default 1000).  This is important when there is a large number of SNPs, because memory could be an issue.")
 
 basicGroup.add_option("-v", "--verbose",
-                  action="store_true", dest="verbose", default=False,
-                  help="Print extra info")
+                      action="store_true", dest="verbose", default=False,
+                      help="Print extra info")
 
 parser.add_option_group(basicGroup)
-#parser.add_option_group(advancedGroup)
+# parser.add_option_group(advancedGroup)
 
 (options, args) = parser.parse_args()
-if len(args) != 1: 
-   parser.print_help()
-   sys.exit()
+if len(args) != 1:
+    parser.print_help()
+    sys.exit()
 
 outFile = args[0]
 
-import sys
-import os
-import numpy as np
-from scipy import linalg
-from pylmm.lmm import calculateKinship
-from pylmm import input
 
-if not options.tfile and not options.bfile and not options.emmaFile: 
-   parser.error("You must provide at least one PLINK input file base (--tfile or --bfile) or an emma formatted file (--emmaSNP).")
+if not options.tfile and not options.bfile and not options.emmaFile:
+    parser.error(
+        "You must provide at least one PLINK input file base (--tfile or --bfile) or an emma formatted file (--emmaSNP).")
 
-if options.verbose: sys.stderr.write("Reading PLINK input...\n")
-if options.bfile: IN = input.plink(options.bfile,type='b')
-elif options.tfile: IN = input.plink(options.tfile,type='t')
-#elif options.pfile: IN = input.plink(options.pfile,type='p')
-elif options.emmaFile: 
-   if not options.numSNPs: parser.error("You must provide the number of SNPs when specifying an emma formatted file.")
-   IN = input.plink(options.emmaFile,type='emma')
-else: parser.error("You must provide at least one PLINK input file base (--tfile or --bfile) or an emma formatted file (--emmaSNP).")
+if options.verbose:
+    sys.stderr.write("Reading PLINK input...\n")
+if options.bfile:
+    IN = input.plink(options.bfile, type='b')
+elif options.tfile:
+    IN = input.plink(options.tfile, type='t')
+# elif options.pfile: IN = input.plink(options.pfile,type='p')
+elif options.emmaFile:
+    if not options.numSNPs:
+        parser.error(
+            "You must provide the number of SNPs when specifying an emma formatted file.")
+    IN = input.plink(options.emmaFile, type='emma')
+else:
+    parser.error(
+        "You must provide at least one PLINK input file base (--tfile or --bfile) or an emma formatted file (--emmaSNP).")
 
 n = len(IN.indivs)
 m = options.computeSize
-W = np.ones((n,m)) * np.nan
+W = np.ones((n, m)) * np.nan
 
 IN.getSNPIterator()
-# Annoying hack to get around the fact that it is expensive to determine the number of SNPs in an emma file
-if options.emmaFile: IN.numSNPs = options.numSNPs
+# Annoying hack to get around the fact that it is expensive to determine
+# the number of SNPs in an emma file
+if options.emmaFile:
+    IN.numSNPs = options.numSNPs
 i = 0
 K = None
 while i < IN.numSNPs:
-   j = 0
-   while j < options.computeSize and i < IN.numSNPs:
-      snp,id = IN.next()
-      if snp.var() == 0:
-	 i += 1
-	 continue
-      W[:,j] = snp
+    j = 0
+    while j < options.computeSize and i < IN.numSNPs:
+        snp, id = IN.next()
+        if snp.var() == 0:
+            i += 1
+            continue
+        W[:, j] = snp
 
-      i += 1
-      j += 1
-   if j < options.computeSize: W = W[:,range(0,j)] 
+        i += 1
+        j += 1
+    if j < options.computeSize:
+        W = W[:, range(0, j)]
 
-   if options.verbose: sys.stderr.write("Processing first %d SNPs\n" % i)
-   if K == None: 
-      try: 
-	 K = linalg.fblas.dgemm(alpha=1.,a=W.T,b=W.T,trans_a=True,trans_b=False) # calculateKinship(W) * j
-      except AttributeError: K = np.dot(W,W.T) 
-   else:
-      try: 
-	 K_j = linalg.fblas.dgemm(alpha=1.,a=W.T,b=W.T,trans_a=True,trans_b=False) # calculateKinship(W) * j
-      except AttributeError: K_j = np.dot(W,W.T)
-      K = K + K_j
+    if options.verbose:
+        sys.stderr.write("Processing first %d SNPs\n" % i)
+    if K is None:
+        try:
+            K = linalg.fblas.dgemm(
+                alpha=1.,
+                a=W.T,
+                b=W.T,
+                trans_a=True,
+                trans_b=False)  # calculateKinship(W) * j
+        except AttributeError:
+            K = np.dot(W, W.T)
+    else:
+        try:
+            K_j = linalg.fblas.dgemm(
+                alpha=1.,
+                a=W.T,
+                b=W.T,
+                trans_a=True,
+                trans_b=False)  # calculateKinship(W) * j
+        except AttributeError:
+            K_j = np.dot(W, W.T)
+        K = K + K_j
 
 K = K / float(IN.numSNPs)
-if options.verbose: sys.stderr.write("Saving Kinship file to %s\n" % outFile)
-np.savetxt(outFile,K)
+if options.verbose:
+    sys.stderr.write("Saving Kinship file to %s\n" % outFile)
+np.savetxt(outFile, K)
 
 if options.saveEig:
-   if options.verbose: sys.stderr.write("Obtaining Eigendecomposition\n")
-   Kva,Kve = linalg.eigh(K)
-   if options.verbose: sys.stderr.write("Saving eigendecomposition to %s.[kva | kve]\n" % outFile)
-   np.savetxt(outFile+".kva",Kva)
-   np.savetxt(outFile+".kve",Kve)
-      
-
-
+    if options.verbose:
+        sys.stderr.write("Obtaining Eigendecomposition\n")
+    Kva, Kve = linalg.eigh(K)
+    if options.verbose:
+        sys.stderr.write(
+            "Saving eigendecomposition to %s.[kva | kve]\n" %
+            outFile)
+    np.savetxt(outFile + ".kva", Kva)
+    np.savetxt(outFile + ".kve", Kve)
